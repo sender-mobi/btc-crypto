1414855e6d2033f7c8cf6a759450855e97759ba2
ASalimov <salimov.artem@gmail.com>
2016-12-15 17:06:29 +0200

    app.java

--- src/main/java/mobi/sender/encryptedsample/App.java
+++ src/main/java/mobi/sender/encryptedsample/App.java
@@ -1,22 +1,13 @@
 package mobi.sender.encryptedsample;
 
-
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
 import java.security.Security;
 
-/**
- * Hello world!
- *
- */
-public class App
-
-{
+public class App{
 
     static {
         Security.addProvider(new BouncyCastleProvider());
     }
-//    public static String pubKeyReceipt = "cbkKanfwPNmUQm7aGzMdt81UoJT9HGebaJLv7U2aUhVh";
     public static void main( String[] args ) throws Exception {
         Security.addProvider(new BouncyCastleProvider());
         LWallet wallet = LWallet.getInstance();
@@ -24,5 +15,4 @@ public class App
         String text = wallet.encrypt(wallet.pubKeyFromString(args[0]), args[1]);
         System.out.println( "Your encr text: " +text);
     }
-
 }
